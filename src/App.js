da70e512713c71b8ce3c27c4e126296f65ed57e7
misksa <misksa17@gmail.com>
2023-02-15 15:47:00 +0500

    add redux, create reducer, create counter cash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+
+    // диспетчер/диспатч изменяет состояние обращаясь к редюсеру
+    const dispatch = useDispatch()
+
+    // полуаем state с помощью хука useSelector, в него передается callback функция в оторую передается state
+    const cash = useSelector(state => state.cash)
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <button
+                onClick={() => addCash(Number(prompt()))}
+            >
+                Пополнить счет
+            </button>
+            <button
+                onClick={() => getCash(Number(prompt()))}
+                style={{marginLeft: "15px"}}
+            >
+                Снять со счета
+            </button>
+        </div>
+        <div style={{marginTop: "15px", fontSize: '2em'}}>
+            {cash}
+        </div>
     </div>
   );
 }
